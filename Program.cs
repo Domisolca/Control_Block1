3f1961562a062dde43685cc3282a757b3aa97ca2
Olga <olya_potapova@mail.ru>
2022-12-01 11:23:46 +0300

    Добавлен этап 5

--- Program.cs
+++ Program.cs
@@ -12,6 +12,14 @@
     }
 }
 
+void WriteArry(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");   
+    }
+}
+
 string[] firstArray = { "Hello", "2", "world", ":-)" };
 int lengthSecond = 0;
 
