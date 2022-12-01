c96fc2765e37423ea65d2c59b6579546043c2e55
Olga <olya_potapova@mail.ru>
2022-12-01 11:21:39 +0300

    Добавлен этап 3-4

--- Program.cs
+++ Program.cs
@@ -1,4 +1,18 @@
-﻿string[] firstArray = { "Hello", "2", "world", ":-)" };
+﻿void FirstToSecondArray(string[] firstArray, string[] secondArray)
+{
+    int j = 0;
+
+    for (int i = 0; i < firstArray.Length; i++)
+    {
+        if (firstArray[i].Length <= 3)
+        {
+            secondArray[j] = firstArray[i];
+            j++;
+        }
+    }
+}
+
+string[] firstArray = { "Hello", "2", "world", ":-)" };
 int lengthSecond = 0;
 
 for (int i = 0; i < firstArray.Length; i++)
@@ -9,4 +23,6 @@ for (int i = 0; i < firstArray.Length; i++)
     }
 }
 
-string[] arraySecond = new string[lengthSecond];
+string[] secondArray = new string[lengthSecond];
+
+FirstToSecondArray(firstArray, secondArray);
