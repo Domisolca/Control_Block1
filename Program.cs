a6657e85071540a1d364c805b8883d1f9632f4da
Olga <olya_potapova@mail.ru>
2022-12-01 11:16:54 +0300

    Добавлен этап 1-2

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿string[] firstArray = { "Hello", "2", "world", ":-)" };
+int lengthSecond = 0;
+
+for (int i = 0; i < firstArray.Length; i++)
+{
+    if (firstArray[i].Length <= 3)
+    {
+        lengthSecond++;
+    }
+}
+
+string[] arraySecond = new string[lengthSecond];
